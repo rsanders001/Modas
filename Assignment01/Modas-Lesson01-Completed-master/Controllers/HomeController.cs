910ba1b7982479eed59c7b43f31a2a35348d6492
rsanders001 <rsanders2k@gmail.com>
2020-01-27 12:47:39 -0600

    Assignment01
    This Is the First Homework Assignment

---
+++ Assignment01/Modas-Lesson01-Completed-master/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modas.Models;
+
+namespace Modas.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index() => "This is POCO 2";
+        //public ViewResult Index() => View();
+        private IEventRepository repository;
+        public HomeController(IEventRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Events);
+    }
+}
---
+++ Assignment01/Modas-Lesson01-Completed-master/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Modas.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool Flagged { get; set; }
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+    }
+}
