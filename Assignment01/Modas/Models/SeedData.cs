7ea7d5b695b9dd0ae70431e45c35beea3854b044
rsanders001 <rsanders2k@gmail.com>
2020-02-03 17:01:56 -0600

    Homework02 Commit
    This Is the Commit for the 2nd Homework Assignment

--- Assignment01/Modas/Models/SeedData.cs
+++ Assignment01/Modas/Models/SeedData.cs
@@ -8,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Modas.Models
 {
-    // static class cannot be instantiated
     public static class SeedData
     {
         public static void EnsurePopulated(IApplicationBuilder app)
@@ -19,10 +18,9 @@ namespace Modas.Models
 
             if (!context.Locations.Any())
             {
-                Locations.Add(new Location { Name = "Front Door" });
                 Locations.Add(new Location { Name = "Family Room" });
+                Locations.Add(new Location { Name = "Front Door" });
                 Locations.Add(new Location { Name = "Rear Door" });
-                // first, add Locations
                 foreach (var l in Locations)
                 {
                     
@@ -35,47 +33,27 @@ namespace Modas.Models
             if (!context.Events.Any())
             {
                 DateTime localDate = DateTime.Now;
-                // subtract 6 months from date
                 DateTime eventDate = localDate.AddMonths(-6);
-                // random numbers will be needed
                 Random rnd = new Random();
-                // loop for each day in the range from 6 months ago to today
                 while (eventDate < localDate)
                 {
-                    // random between 0 and 5 determines the number of events to occur on a given day
                     int num = rnd.Next(0, 6);
-
-                    // a sorted list will be used to store daily events sorted by time
-                    // each time an eventr is added, the list is re-sorted
                     SortedList<DateTime, Event> dailyEvents = new SortedList<DateTime, Event>();
-                    // for loop to generate times for each event
                     for (int i = 0; i < num; i++)
                     {
-                        // random between 0 and 23 for hour of the day
                         int hour = rnd.Next(0, 24);
-                        // random between 0 and 59 for minute of the day
                         int minute = rnd.Next(0, 60);
-                        // random between 0 and 59 for seconds of the day
                         int second = rnd.Next(0, 60);
-
-                        // random location
                         int loc = rnd.Next(0, Locations.Count());
-
-                        // generate event date/time
                         DateTime x = new DateTime(eventDate.Year, eventDate.Month, eventDate.Day, hour, minute, second);
-                        // create event from date/time and location
                         Event e = new Event { TimeStamp = x, Flagged = false, Location = context.Locations.FirstOrDefault(l => l.Name == Locations[loc].Name) };
-                        // add daily events to sorted list
                         dailyEvents.Add(e.TimeStamp, e);
                     }
-                    // using sorted list for daily events, add to event list
                     foreach (var item in dailyEvents)
                     {
                         context.Events.Add(item.Value);
-                        // save changes to database
                         context.SaveChanges();
                     }
-                    // add 1 day
                     eventDate = eventDate.AddDays(1);
                 }
             }
